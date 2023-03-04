103b65a47cece4bbaf35d3f2f8fc02f09e403243
Ally_08 <leslyaea22@gmail.com>
2023-03-04 16:49:22 -0600

    hjhh

--- public/interfazPublica.php
+++ public/interfazPublica.php
@@ -10,36 +10,12 @@
 </head>
 </span>
 <body>
+
     <form action="" method="post">
-
-        <?php
-            if(isset($_POST['buscar'])){
-                $busqueda = $_POST['busqueda'];
-                $productos =simplexml_load_file("../xml/productos.xml");
-
-                $productosFiltro = array_filter($productos,function($producto) use ($busqueda){
-                    return strpos(strtolower($producto['nombre']), strtolower($busqueda)) !== false;
-                } );
-            }else{
-                $productosFiltro = $productos;
-            }
-        ?>
-
         <input type="text" placeholder="Producto a buscar..." class="search-box" name="" id="" value="">
         <button type="submit" name="buscar" id="buscar"> <i class="fa-solid fa-magnifying-glass icon"></i> </button>
     </form>
-        
-            <?php
-                if(count($productosFiltro) > 0){
-                    foreach($productosFiltro as $producto){
-                        echo "" . $producto["nombre"] . "<br>";
-                    }
-                }else{
-                    echo "Producto no disponible";
-                }
-            ?>
-
-
+    
     <div id="product-container">
             <?php $productos =simplexml_load_file("../xml/productos.xml");
 					foreach ($productos->producto as $prod) {					
